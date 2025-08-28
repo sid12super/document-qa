2a528990a5a099613f8d3b5f087364cc51e65c9d
Siddhant <73212658+sid12super@users.noreply.github.com>
2025-08-28 15:34:57 +0000

    test 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📄 Your Document question answering")
+st.title("📄 Sid's Document question answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "Using GPT-5-nano"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
