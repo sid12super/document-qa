44a823b6f9033cd3dc66cb76f1073b0f1102b5d9
Siddhant <73212658+sid12super@users.noreply.github.com>
2025-08-30 00:57:01 +0000

    hw

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ else:
 
     # Let the user upload a file via `st.file_uploader`.
     uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+        "Upload a document (.txt or .pdf)", type=("txt", "pdf")
     )
 
     # Ask the user for a question via `st.text_area`.
